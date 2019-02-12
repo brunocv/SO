f4bdb525d574848df482da0f93a8cfbae7704643
brunocv <brunocveloso@sapo.pt>
2019-02-12 15:29:21 +0000

    exe1

--- guiao1/10MB.c
+++ guiao1/10MB.c
@@ -1,5 +1,21 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
 
+int main(int argc, char** argv){
+    int i=0;
+    char c='a';
+    
+    int file = open(argv[1], O_CREAT | O_TRUNC |O_RDONLY |O_WRONLY , S_IRUSR | S_IRGRP | S_IROTH);
 
+    while (i<100000){
+        write(file,&c,1);
+        i++;
+    }
+    
+    close(file);
+
+    return 0;
+}
 
